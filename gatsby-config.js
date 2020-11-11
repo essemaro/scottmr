053076658a7b0e9dc1266a706cdf2c344a0fe319
Scott Robertson <fuzzy343@gmail.com>
2020-11-11 16:34:38 -0500

    Change to gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Scott Robertson Portfolio`,
+    siteTitleAlt: `Scott Robertson's Portfolio Site`,
   },
   plugins: [
     {
@@ -57,6 +57,6 @@ module.exports = {
         openAnalyzer: false,
       },
     },
-    `gatsby-plugin-fontawesome-css`,
+    `gatsby-plugin-fontawesome-css`
   ].filter(Boolean),
 }
